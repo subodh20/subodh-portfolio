20e05f9e3cea8fdc7df936b51dc2e5840152d946
Subodh Tiwari <subodhtiwari360@gmail.com>
2025-12-14 17:43:56 +1100

    Basic restructuring of the nav bar and add brief intro component

--- src/components/NavLinkComponent.tsx
+++ src/components/NavLinkComponent.tsx
@@ -1,5 +1,12 @@
-type NavLinkComponentProps = {};
-const NavLinkComponent = (props: NavLinkComponentProps): JSX.Element => {
-  return <>This is nav conponent</>;
+import { NavLink } from "react-router-dom";
+type NavLinkComponentProps = {
+  to: string;
+  title: string;
+};
+const NavLinkComponent = ({
+  to,
+  title,
+}: NavLinkComponentProps): JSX.Element => {
+  return <NavLink to={to}>{title}</NavLink>;
 };
 export default NavLinkComponent;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,26 @@
+const navItems = [
+  { label: "About", href: "#about" },
+  { label: "Skills", href: "#skills" },
+  { label: "Projects", href: "#projects" },
+  { label: "Blog", href: "#blog" },
+  { label: "Contact", href: "#contact" },
+];
 const Navbar = () => {
-  return <>This is navbar</>;
+  return (
+    <nav className="fixed top-0 left-0 right-0 z-50">
+      <div className="container mx-auto px-6">
+        <div className="flex items-center justify-between h-16 md:h-20">
+          <a href="#">logo</a>
+          <div className=" md-flex items-center gap-8">
+            {navItems.map(({ label, href }) => (
+              <a key={label} href={href}>
+                {label}
+              </a>
+            ))}
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 };
 export default Navbar;
