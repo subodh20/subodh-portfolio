3e4d0aab79d7e06540a97d9dec81204fb2422fdd
Subodh Tiwari <subodhtiwari360@gmail.com>
2025-12-14 17:43:56 +1100

    Basic restructuring of the nav bar and add brief intro component

---
+++ src/components/BriefIntro.jsx
@@ -0,0 +1,26 @@
+const BriefIntro = () => {
+  return (
+    <section className="min-h-screen flex items-center justify-center relative overflow-hidden">
+      <div className="container mx-auto px-6 relative z-10">
+        <div className="max-w-4xl mx-auto text-center ">
+          {/* Introduction Section */}
+          <h1>
+            <span>Hi, I'm </span>
+            <span>Subodh Tiwari</span>
+          </h1>
+          <p>Full Stack Developer</p>
+          {/* Description */}
+          <p>
+            I build responsive and user-friendly applications with modern
+            technologies.
+          </p>
+          {/* Download resume button */}
+          <div></div>
+          {/* My social links */}
+          <div></div>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default BriefIntro;
--- src/components/MainApp.tsx
+++ src/components/MainApp.tsx
@@ -2,9 +2,13 @@ import About from "./About";
 import Blog from "./Blog";
 import Contact from "./Contact";
 import Skills from "./Skills";
+import Navbar from "./Navbar";
+import BriefIntro from "./BriefIntro";
 const MainApp = () => {
   return (
     <div>
+      <Navbar />
+      <BriefIntro />
       <About title="This is about page" />
       <Skills />
       <Blog />
