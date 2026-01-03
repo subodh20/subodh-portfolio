b88d6df7ddd23c3660ce7478b1c6b5c0ad2d4e02
Subodh Tiwari <subodhtiwari360@gmail.com>
2026-01-03 14:22:11 +1100

    Add projects section

---
+++ src/pages/Projects.tsx
@@ -0,0 +1,45 @@
+const myProjects = [
+  {
+    title: "Old Portfolio",
+    image: "",
+    srcCode: "",
+    liveLink: "https://subodh-portfolio.netlify.app",
+    techStack: ["React", "Tailwind", "SCSS"],
+    description:
+      "This is the very first portfolio app developed while I was learning React",
+  },
+];
+import { FaExternalLinkAlt } from "react-icons/fa";
+import ProjectsCard from "../components/ProjectsCard";
+const Projects = () => {
+  return (
+    <section id="projects" className="py-24 relative">
+      <div className="container max-auto px-6 ">
+        <div className="text-center mb-16">
+          <p className="text-accent font-mono text-sm mb-4 tracking-wider uppercase">
+            Portfolio
+          </p>
+          <h2 className="text-4xl md:text-5xl font-bold mb-4">
+            Featured Projects
+          </h2>
+          <p className="text-muted-foreground max-w-2xl max-auto">
+            A selection of projects that showscase my skills and passion for
+            creating quality software applications
+          </p>
+        </div>
+        <div className="space-y-24 max-w-6xl mx-auto">
+          {myProjects.map((project, index) => (
+            <ProjectsCard key={index} index={index} project={project} />
+          ))}
+        </div>
+        <div className="text-center mt-16">
+          <button className="flex flex-row text-center border border-input bg-background hover:bg-accent hover:text-accent-foreground h-11 rounded-md px-8">
+            View All Projects
+            <FaExternalLinkAlt className="ml-2 h-4 w-4" />
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Projects;
--- src/pages/Skills.tsx
+++ src/pages/Skills.tsx
@@ -55,7 +55,7 @@ const Skills = () => {
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-6xl mx-auto">
           {skillList.map((item, index) => (
-            <SkillsCard key={item.title} index={index} item={item} />
+            <SkillsCard key={index} index={index} item={item} />
           ))}
         </div>
       </div>
